e06c7fea4a12bc70c852e7da7baefe37c6d6e9e3
Jacob Balin <balinj@oregonstate.edu>
2021-03-07 15:19:40 -0800

    Implimented fb function that prints Buzz instead of the numbnerr if the number is divisible by 5.

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -2,10 +2,14 @@ def fb(x):
     for i in range(1, 101):
         if(i % 3 == 0):
             print("Fizz")
+        elif(i % 5 == 0):
+            print("Buzz")
         else:
             print(i)
     
     if(x % 3 == 0):
         return("Fizz")
+    elif(x % 5 == 0):
+        return("Buzz")
     else:
         return x
