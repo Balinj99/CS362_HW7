70a774ab16db124f3acd0db8d0bb2d3a60ecf1a0
Jacob Balin <balinj@oregonstate.edu>
2021-03-07 15:15:57 -0800

    Wrote third test to test numbers divisible by 5 which should return Buzz instead of the number.

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -9,6 +9,10 @@ class TestCase(unittest.TestCase):
     def test2(self):
         result = fizzBuzz.fb(3)
         self.assertEqual(result, "Fizz")
+    
+    def test3(self):
+        result = fizzBuzz.fb(5)
+        self.assertEqual(result, "Buzz")
 
 if __name__ == "__main__":
     unittest.main()   
