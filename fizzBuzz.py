4530fa57eb6c79c812265381af484c4dedb1e3f0
Jacob Balin <balinj@oregonstate.edu>
2021-03-07 15:28:54 -0800

    Implimented fb so that if a number is divisible by 3 and 5, FizzBuzz is output instead of the number.

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,13 +1,17 @@
 def fb(x):
     for i in range(1, 101):
-        if(i % 3 == 0):
+        if(i % 3 == 0 and i % 5 == 0):
+            print("FizzBuzz")
+        elif(i % 3 == 0):
             print("Fizz")
         elif(i % 5 == 0):
             print("Buzz")
         else:
             print(i)
     
-    if(x % 3 == 0):
+    if(x % 3 == 0 and x % 5 == 0):
+        return("FizzBuzz")
+    elif(x % 3 == 0):
         return("Fizz")
     elif(x % 5 == 0):
         return("Buzz")
