394d00a143bc90256e15078b0fc531a567c7b46f
Jacob Balin <balinj@oregonstate.edu>
2021-03-07 16:13:18 -0800

    Implimented ly function to return that a year is a leap year if it is divisible by 100 only if it is also divisible by 400.

--- leapYear.py
+++ leapYear.py
@@ -2,8 +2,11 @@ def ly(x):
     if(x % 4 == 0 and x % 100 != 0):
         msg = "Input year is a leap year."
         print(msg)
-        return(msg)
+    elif(x % 100 == 0 and x % 400 == 0):
+        msg = "Input year is a leap year."
+        print(msg)
     else:
         msg = "Input year is not a leap year."
         print(msg)
-        return(msg)
+
+    return(msg)
