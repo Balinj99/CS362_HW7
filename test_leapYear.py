711dfbf85b6790830ba8a63ae77f3a5df6889701
Jacob Balin <balinj@oregonstate.edu>
2021-03-07 16:02:48 -0800

    Wrote test to test that a year divisible by 100 is not a leap year.

--- test_leapYear.py
+++ test_leapYear.py
@@ -4,7 +4,11 @@ import leapYear
 class TestCase(unittest.TestCase):
     def test1(self):
         result = leapYear.ly(2004)
-        self.assertEqual(result, "That year is a leap year.")
+        self.assertEqual(result, "Input year is a leap year.")
+
+    def test2(self):
+        result = leapYear.ly(2100)
+        self.assertEqual(result, "Input year is not a leap year.")
 
 if __name__ == "__main__":
     unittest.main()   
