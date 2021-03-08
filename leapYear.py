7fd48dc362e09dc72b56d9b32994ec7ce4d80c4a
Jacob Balin <balinj@oregonstate.edu>
2021-03-07 16:05:04 -0800

    Implimented ly function to not print a year is a leap year if it is divisible by 100 even if it is divisible by 4.

--- leapYear.py
+++ leapYear.py
@@ -1,5 +1,9 @@
 def ly(x):
-    if(x % 4 == 0):
+    if(x % 4 == 0 and x % 100 != 0):
         msg = "Input year is a leap year."
+        print(msg)
+        return(msg)
+    else:
+        msg = "Input year is not a leap year."
         print(msg)
         return(msg)
